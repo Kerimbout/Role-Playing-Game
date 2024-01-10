435da03eba8ad3008af543fb305812362e3b3859
Kerimbout <52263565+Kerimbout@users.noreply.github.com>
2024-01-10 06:54:13 +0300

    initialize buttons

--- script.js
+++ script.js
@@ -18,3 +18,7 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
