55c31bfe3986b31a5a0a624623bf93f59a132fed
Kerimbout <52263565+Kerimbout@users.noreply.github.com>
2024-01-11 14:26:10 +0300

    In your goStore() function, update the onclick property for each button

--- script.js
+++ script.js
@@ -24,16 +24,20 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 // Creating functions
-function goStore() { 
-    button1.innerText = "Buy 10 health (10 gold)"; //The innerText property controls the text that appears in an HTML element.
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
+function goStore() {
+    //button1.innerText = "Buy 10 health (10 gold)"; //The innerText property controls the text that appears in an HTML element.
+    //button2.innerText = "Buy weapon (30 gold)";
+    //button3.innerText = "Go to town square";
+
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
 }
 
 function goCave() {
     console.log("Going to cave.");
 }
-    
+
 function fightDragon() {
     console.log("Fighting dragon.");
 }
